b79981027758bb7cbed06de3f30570b1812541de
Felipe Restrepo <manuel.restrepo@carvajal.com>
2022-04-14 08:02:46 -0500

    Test for better prime checker

--- prueba-tecnica-1/test_fetcher.py
+++ prueba-tecnica-1/test_fetcher.py
@@ -7,10 +7,10 @@ class TestUser(unittest.TestCase):
     def test_fetcher_is_prime(self):
         fetcher = Fetcher()
         for n in (1,2,3,5,7):
-            self.assertEqual(fetcher.is_prime(n), 0)
+            self.assertEqual(fetcher.is_prime(n), True)
         for n in (4,6,9,10):
-            self.assertEqual(fetcher.is_prime(n), 1)
-        self.assertEqual(fetcher.is_prime("n"), 0)
+            self.assertEqual(fetcher.is_prime(n), False)
+        self.assertEqual(fetcher.is_prime("n"), False)
     
     def test_fetcher_get_uri(self):
         fetcher = Fetcher()
